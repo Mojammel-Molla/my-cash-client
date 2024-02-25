6c5f04929a0e3d11dbe20c40375f7801ed0221b8
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-02-26 01:04:36 +0600

    Navbar with customize logo created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './pages/home/navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className="text-3xl text-orange-800">This is Home page</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import { NavLink } from 'react-router-dom';
+import LogoImage from '../../../assets/cash-logo.png';
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <NavLink>
+        <li>Home</li>
+      </NavLink>
+      <NavLink>
+        <li>Sent Money</li>
+      </NavLink>
+      <NavLink>
+        <li>Received Money</li>
+      </NavLink>
+      <NavLink>
+        <li>Transactions</li>
+      </NavLink>
+      <NavLink>
+        <li>Balance</li>
+      </NavLink>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-medium"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <div className="flex">
+          <img className="h-10 w-16" src={LogoImage} alt="" />
+          <a className="text-[#0074d9]  text-2xl font-bold">My Cash</a>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal space-x-5 px-1 font-medium">
+          {navLinks}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Log In</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
